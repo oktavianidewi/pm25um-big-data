63492c2e57897a6333f538ddfe9e8f1a5d916a81
oktavianidewi <oktaviani.dewi@gmail.com>
2025-11-11 10:41:34 +0700

    update setting

--- infra/hdfs-docker-cluster/start-hdfs.sh
+++ infra/hdfs-docker-cluster/start-hdfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d "/opt/hadoop/data/nameNode/current" ]; then
     echo "Formatting NameNode..."
-    hdfs namenode -format
+    hdfs namenode -format -force -nonInteractive;
 fi
 hdfs namenode
